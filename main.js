3bbfbd4159ce471cde70a5b180b1360e6bc41c6e
ManhQuang <nguyenquangmanh.6264@gmail.com>
2024-06-07 16:42:46 +0700

    audio

--- main.js
+++ main.js
@@ -4,13 +4,7 @@ onload = () => {
       clearTimeout(c);
   }, 1000);
 };
-
-document.addEventListener("DOMContentLoaded", () => {
-  const flower = document.querySelector(".flower");
-  const messageBox = document.querySelector(".message-box");
-
-  // Display message box after 5 seconds
+// Display message box after 5 seconds
 setTimeout(() => {
     document.querySelector('.message-box').classList.add('show');
-}, 10000); // Adjust the timing as needed
-});
+}, 10000); // Adjust the timing as needed 
