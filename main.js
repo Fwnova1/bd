7692f24dfa7633c42e52276c608a24c8961a2851
ManhQuang <nguyenquangmanh.6264@gmail.com>
2024-06-07 14:34:33 +0700

    br

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 onload = () => {
-    const c = setTimeout(() => {
+  const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
       clearTimeout(c);
-    }, 1000);
-  };
+  }, 1000);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const flower = document.querySelector(".flower");
+  const messageBox = document.querySelector(".message-box");
+
+  // Display message box after 5 seconds
+setTimeout(() => {
+    document.querySelector('.message-box').classList.add('show');
+}, 10000); // Adjust the timing as needed
+});
